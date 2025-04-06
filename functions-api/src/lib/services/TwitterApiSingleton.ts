4a40b96c63cbb428fd406611191a377d94edcbdb
okmethod <okmethod2020@gmail.com>
2025-04-06 19:07:06 +0900

    :+1: adjust TwitterApiSingletonMap

--- functions-api/src/lib/services/TwitterApiSingleton.ts
+++ functions-api/src/lib/services/TwitterApiSingleton.ts
@@ -1,43 +1,34 @@
 import { TwitterApi, type TwitterApiTokens } from "twitter-api-v2";
-import { defineString } from "firebase-functions/params";
+import { getEnv } from "../utils/getEnv.js";
 
-const apiKey = defineString("TWITTER_API_KEY");
-const apiSecretKey = defineString("TWITTER_API_SECRET_KEY");
-const accessToken = defineString("TWITTER_ACCESS_TOKEN");
-const accessTokenSecret = defineString("TWITTER_ACCESS_TOKEN_SECRET");
-
-class TwitterApiSingleton {
-  private static instance: TwitterApi | null = null;
+class TwitterApiSingletonMap {
+  private static instances: Record<string, TwitterApi> = {};
 
   // Singleton pattern
   private constructor() {}
 
-  public static getInstance(): TwitterApi {
-    if (!TwitterApiSingleton.instance) {
-      const tokens = TwitterApiSingleton.getCredentials();
-      TwitterApiSingleton.instance = TwitterApiSingleton.initializeInstance(tokens);
+  public static getInstance(accountPassphrase: string): TwitterApi {
+    if (!(accountPassphrase in TwitterApiSingletonMap.instances)) {
+      const tokens = TwitterApiSingletonMap.getTokens(accountPassphrase);
+      const instance = TwitterApiSingletonMap.initializeInstance(tokens);
+      TwitterApiSingletonMap.instances[accountPassphrase] = instance;
     }
-    return TwitterApiSingleton.instance;
+    return TwitterApiSingletonMap.instances[accountPassphrase];
   }
 
-  private static getCredentials(): TwitterApiTokens {
-    const getEnvValue = (key: string, fallback: string | undefined) =>
-      process.env.NODE_ENV === "production" ? key : fallback;
-
-    const apiKeyValue = getEnvValue(apiKey.value(), process.env.TWITTER_API_KEY);
-    const apiSecretKeyValue = getEnvValue(apiSecretKey.value(), process.env.TWITTER_API_SECRET_KEY);
-    const accessTokenValue = getEnvValue(accessToken.value(), process.env.TWITTER_ACCESS_TOKEN);
-    const accessTokenSecretValue = getEnvValue(accessTokenSecret.value(), process.env.TWITTER_ACCESS_TOKEN_SECRET);
-
-    if (!apiKeyValue || !apiSecretKeyValue || !accessTokenValue || !accessTokenSecretValue) {
-      throw new Error("Failed to get Twitter API credentials");
-    }
-    return {
-      appKey: apiKeyValue,
-      appSecret: apiSecretKeyValue,
-      accessToken: accessTokenValue,
-      accessSecret: accessTokenSecretValue,
+  private static getTokens(accountPassphrase: string): TwitterApiTokens {
+    const tokenMap: Record<string, TwitterApiTokens> = {
+      synchron: {
+        appKey: getEnv("TWITTER_API_KEY"),
+        appSecret: getEnv("TWITTER_API_SECRET_KEY"),
+        accessToken: getEnv("TWITTER_ACCESS_TOKEN"),
+        accessSecret: getEnv("TWITTER_ACCESS_TOKEN_SECRET"),
+      },
     };
+    if (!(accountPassphrase in tokenMap)) {
+      throw new Error("Unkown accountPassphrase");
+    }
+    return tokenMap[accountPassphrase];
   }
 
   private static initializeInstance(tokens: TwitterApiTokens): TwitterApi {
@@ -51,4 +42,4 @@ class TwitterApiSingleton {
   }
 }
 
-export default TwitterApiSingleton;
+export default TwitterApiSingletonMap;
