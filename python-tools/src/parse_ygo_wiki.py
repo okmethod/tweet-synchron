9349711d8c80c3096436716eaeaeee1d7b11e662
okmethod <okmethod2020@gmail.com>
2025-04-11 23:49:17 +0900

    +1: uv add requests

--- python-tools/src/parse_ygo_wiki.py
+++ python-tools/src/parse_ygo_wiki.py
@@ -1,3 +1,5 @@
+import requests
+
 from src.utils.url_parser import encode_euc_jp 
 
 
@@ -5,7 +7,10 @@ def parse_ygo_wiki(page_name: str) -> None:
     param = encode_euc_jp(page_name)
     url = f"https://yugioh-wiki.net/index.php?{param}"
 
-    print(url)
+    response = requests.get(url)
+    response.raise_for_status()
+
+    print(response.text)
 
 
 if __name__ == "__main__":
