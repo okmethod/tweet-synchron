e0ec402b6c56d2c65a56aff21574967acdaf37e2
okmethod <okmethod2020@gmail.com>
2025-04-12 00:54:38 +0900

    :recycle: rename to parse_ygo_wiki_table.py

--- python-tools/src/parse_ygo_wiki_table.py
+++ python-tools/src/parse_ygo_wiki_table.py
@@ -5,7 +5,7 @@ from src.utils.html_parser import StyleTableParser
 from src.utils.regex_processer import remove_asterisk_number, is_enclosed_in_brackets
 
 
-def parse_ygo_wiki(page_name: str) -> list[str]:
+def parse_ygo_wiki_table(page_name: str) -> list[str]:
     param = encode_euc_jp(page_name)
     url = f"https://yugioh-wiki.net/index.php?{param}"
 
@@ -20,9 +20,9 @@ def parse_ygo_wiki(page_name: str) -> list[str]:
 
 
 if __name__ == "__main__":
-    page_name = "融合モンスター"
+    page_name = "シンクロモンスター"
     monster_names = [
-        name for name in (remove_asterisk_number(name) for name in parse_ygo_wiki(page_name))
+        name for name in (remove_asterisk_number(name) for name in parse_ygo_wiki_table(page_name))
         if is_enclosed_in_brackets(name)
     ]
     for name in monster_names:
